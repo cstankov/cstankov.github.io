0e4f51dde57d2d79416c7be81de6970b7908b338
cstankov <cole_stankov@sfu.ca>
2020-09-06 19:24:06 -0700

    added links to school project section

--- website.js
+++ website.js
@@ -1,3 +1,5 @@
+//  https://www.w3schools.com/howto/howto_js_slideshow.asp
+
 var slideIndex = 1;
 showSlides(slideIndex);
 
@@ -25,10 +27,6 @@ function showSlides(n) {
   for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";  
   }
-  
-  // for (i = 0; i < dots.length; i++) {
-  //     dots[i].className = dots[i].className.replace(" active", "");
-  // }
   
   slides[slideIndex-1].style.display = "block";  
   dots[slideIndex-1].className += " active";
